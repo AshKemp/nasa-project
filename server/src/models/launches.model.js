7d428c6f6df7b0e7f010f2fbdd474dc8a41db3fd
AshKemp <kmuraliashwin@gmail.com>
2022-09-17 19:31:25 +0530

    Update model and controller to create launch

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,39 +1,50 @@
-// const launches = require("./launches.mongo");
+const launchesDatabase = require("./launches.mongo");
+const planets = require("./planets.mongo");
+const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launches = new Map();
-
-let latestFlightNumber = 100;
-
-let launch = {
-  flightNumber: 100,
-  mission: "Kepler Exploration X",
-  rocket: "Explorer IS1",
-  launchDate: new Date("December 27,2030"),
-  target: "Kepler-442 b",
-  customers: ["ZTM", "NASA"],
-  upcoming: true,
-  success: true,
-};
-
-launches.set(launch.flightNumber, launch);
-launches.get(launch.flightNumber);
-
-function getAllLaunches() {
-  return Array.from(launches.values());
+async function getLatestFlightNumber() {
+  const latestLaunch = await launchesDatabase.findOne().sort("-flightNumber");
+  console.log("Latest Launch: ");
+  if (!latestLaunch) {
+    return DEFAULT_FLIGHT_NUMBER;
+  }
+  return latestLaunch.flightNumber;
 }
 
-function addNewLaunch(launch) {
-  latestFlightNumber++;
-  launches.set(
-    latestFlightNumber,
-    Object.assign(launch, {
-      flightNumber: latestFlightNumber,
-      customers: ["ZTM", "NASA"],
-      upcoming: true,
-      success: true,
-    })
+async function getAllLaunches() {
+  return await launchesDatabase.find(
+    {},
+    {
+      __id: 0,
+      __v: 0,
+    }
   );
-  return launch;
+}
+
+async function saveLaunch(launch) {
+  const planet = await planets.findOne({ keplerName: launch.target });
+  if (!planet) {
+    throw new Error("Planet not found!!");
+  }
+  await launchesDatabase.findOneAndUpdate(
+    {
+      flightNumber: launch.flightNumber,
+    },
+    launch,
+    { upsert: true }
+  );
+}
+
+async function scheduleNewLaunch(launch) {
+  const newFlightNumber = (await getLatestFlightNumber()) + 1;
+  const newLaunch = Object.assign(launch, {
+    customers: ["ZTM", "NASA"],
+    upcoming: true,
+    success: true,
+    flightNumber: newFlightNumber,
+  });
+
+  await saveLaunch(newLaunch);
 }
 
 function isLaunchExists(launchId) {
@@ -49,7 +60,7 @@ function abortLaunchById(launchId) {
 
 module.exports = {
   getAllLaunches,
-  addNewLaunch,
+  scheduleNewLaunch,
   isLaunchExists,
   abortLaunchById,
 };
