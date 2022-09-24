95e8dacea4511042119bb3a1e0c80176ab32102e
AshKemp <kmuraliashwin@gmail.com>
2022-09-24 16:30:08 +0530

    version node apis

--- server/src/app.js
+++ server/src/app.js
@@ -3,10 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
-const planetsRouter = require("./routes/planets/planets.router");
-const launchesRouter = require("./routes/launches/launches.router");
-
 const app = express();
+const api = require("./routes/api");
 
 app.use(cors({ origin: "http://localhost:3000" }));
 app.use(morgan("combined"));
@@ -14,8 +12,7 @@ app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use("/planets", planetsRouter);
-app.use("/launches", launchesRouter);
+app.use("/v1", api);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
